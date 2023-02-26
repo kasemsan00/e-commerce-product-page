ef24332aba96e2a003e896c33151d81fdd59c349
Kasemsan <kasemsan.cho@gmail.com>
2023-02-26 12:36:54 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Logo from "./components/Logo";
 import Menu from "./components/Menu";
 import Profile from "./components/Profile";
 import Cart from "./components/Cart";
+import ProductPicture from "./components/ProductPicture/ProductPicture";
+import ProductDetail from "./components/ProductDetail";
+import ModalPicture from "./components/ProductPicture/ModalPicture";
 
 function App() {
     return (
@@ -12,6 +15,10 @@ function App() {
                 <Menu />
                 <Cart />
                 <Profile />
+            </div>
+            <div className="main-section">
+                <ProductPicture />
+                <ProductDetail />
             </div>
         </div>
     );
---
+++ src/components/ProductDetail.tsx
@@ -0,0 +1,12 @@
+export default function ProductDetail() {
+    return (
+        <div className="product-detail">
+            <label>SNEAKER COMPANY</label>
+            <h3>Fall Limited Edition Sneakers</h3>
+            <p>
+                These low-profile sneakers are your prefect casual wear companion. Featuring a durable rubber outer sole, they'll
+                withstand everything the weather can offer.
+            </p>
+        </div>
+    );
+}
