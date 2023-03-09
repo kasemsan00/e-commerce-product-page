3ec5b1214303b7c960f7052a60d9d8fa11a72796
kasemsan00 <kasemsan.cho@gmail.com>
2023-03-09 09:13:19 +0700

    .

--- src/components/ProductDetail/ProductDetail.tsx
+++ src/components/ProductDetail/ProductDetail.tsx
@@ -1,4 +1,41 @@
 import Price from "./Price";
+import React, { useState } from "react";
+const minValue = 1;
+const maxValue = 100;
+
+const AddItem = () => {
+    const [countItem, setCountItem] = useState(1);
+    const handleIncreaseDecrease = (e: React.MouseEvent<HTMLSpanElement>) => {
+        const indicator = (e.target as HTMLDivElement).textContent;
+        if (indicator === "-") {
+            let counter = countItem - 1;
+            if (counter <= minValue) {
+                counter = minValue;
+            }
+            setCountItem(counter);
+        }
+        if (indicator === "+") {
+            let counter = countItem + 1;
+            if (counter >= maxValue) {
+                counter = maxValue;
+            }
+            setCountItem(counter);
+        }
+    };
+    const handleAddToCart = () => {
+        console.log("add to cart");
+    };
+    return (
+        <div className={"add-item-section"}>
+            <div className={"add-item"}>
+                <span onClick={handleIncreaseDecrease}>-</span>
+                <input type="number" min={minValue} max={maxValue} value={countItem} onChange={(e) => {}} />
+                <span onClick={handleIncreaseDecrease}>+</span>
+            </div>
+            <input className={"add-to-cart"} type="button" value="Add to cart" onClick={handleAddToCart} />
+        </div>
+    );
+};
 
 export default function ProductDetail() {
     return (
@@ -10,6 +47,7 @@ export default function ProductDetail() {
                 withstand everything the weather can offer.
             </p>
             <Price />
+            <AddItem />
         </div>
     );
 }
